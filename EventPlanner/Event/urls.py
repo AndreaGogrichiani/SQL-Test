eef637c8249e9b3d50910fc0a61c32114cd1f60e
AndreaGogrichiani <justthepumpkin@gmail.com>
2024-03-09 14:43:59 +0400

    adding add event page

--- EventPlanner/Event/urls.py
+++ EventPlanner/Event/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    path("add_event/", views.add_event),
     path("errorpage/", views.errorpage),
     path("<str:title>/", views.eventinfo),
 ]
--- EventPlanner/Event/views.py
+++ EventPlanner/Event/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, redirect
 from .models import Events
 from django.http import HttpResponse
+from .forms import EventForm
+
+def add_event(request):
+    return render(request, 'Event/add_event.html')
 
 def eventinfo(request, title):
+    try:
+        event = Events.objects.get(title__iexact=title)
+        return render(request, 'Event/eventinfo.html', {'event': event})
+    except Events.DoesNotExist:
+        return HttpResponse("Event not found")
 
-    event_list = Events.objects.all()
-
-    titles = [event.title for event in event_list]
-
-    return render(request, 'Event/eventinfo.html', {'titles': titles})
 
 def errorpage(request):
     return HttpResponse("ERROR")
