f4192b840fdd2eaa59fd060c9884e8dd9cf6bb8e
AndreaGogrichiani <justthepumpkin@gmail.com>
2024-03-09 15:27:13 +0400

    Successfully added event page, and finished the project

--- EventPlanner/Event/urls.py
+++ EventPlanner/Event/urls.py
@@ -2,7 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("add_event/", views.add_event),
+    path("submitted/", views.submitted),
+    path('add_event/', views.add_event),
     path("errorpage/", views.errorpage),
     path("<str:title>/", views.eventinfo),
 ]
--- EventPlanner/Event/views.py
+++ EventPlanner/Event/views.py
@@ -1,19 +1,34 @@
 from django.shortcuts import render, redirect
 from .models import Events
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .forms import EventForm
 
+
 def add_event(request):
-    return render(request, 'Event/add_event.html')
+    submitted = False
+    form = EventForm
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/events/submitted/')
+    else:
+        form = EventForm()
+        if 'submitted' in request.GET:
+            submitted = True
+
+    return render(request, 'Event/add_event.html', {'form': form, 'submitted': submitted})
 
 def eventinfo(request, title):
     try:
         event = Events.objects.get(title__iexact=title)
         return render(request, 'Event/eventinfo.html', {'event': event})
     except Events.DoesNotExist:
-        return HttpResponse("Event not found")
+        return redirect('/events/errorpage/')
 
 
 def errorpage(request):
     return HttpResponse("ERROR")
 
+def submitted(request):
+    return HttpResponse("Submitted Successfully")
